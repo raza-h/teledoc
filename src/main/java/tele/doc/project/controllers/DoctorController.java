b70f65b9b8e83cf39e3388803cd31a9d44b59680
raza-h <razah12145@gmail.com>
2022-11-25 18:49:48 +0500

    use cases 4 and 8 implemented.

--- src/main/java/tele/doc/project/controllers/DoctorController.java
+++ src/main/java/tele/doc/project/controllers/DoctorController.java
@@ -1,6 +1,5 @@
 package tele.doc.project.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,16 +9,9 @@ import tele.doc.project.repositories.AppointmentRepository;
 import tele.doc.project.repositories.DoctorRepository;
 import tele.doc.project.repositories.PrescriptionRepository;
 import tele.doc.project.systems.others.DecideAppointmentSystem;
-import tele.doc.project.systems.regloc.DoctorRegister;
-import javax.persistence.*;
+import tele.doc.project.systems.others.PrescriptionCreationSystem;
 import java.io.IOException;
-import java.sql.Time;
-import java.text.DateFormat;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Objects;
-
 import java.util.Optional;
 
 @Controller
@@ -27,7 +19,8 @@ public class DoctorController {
     private final DoctorRepository dr;
     private final AppointmentRepository ar;
     private final PrescriptionRepository pr;
-    long  id;
+    long id;
+    PrescriptionCreationSystem pcs;
     Status status;
     DecideAppointmentSystem das;
 
@@ -69,12 +62,6 @@ public class DoctorController {
             Appointment app = a1.get();
             float result = Float.parseFloat(amount);
             app.setAmount(result);
-//            String h= t1.split(":")[0];
-//            String m=t1.split(":")[1];
-//            int hours=Integer.parseInt(h);
-//            int minutes=Integer.parseInt(m);
-//            Time t=new Time(hours,minutes,0);
-//            app.setTime(t);
             app.setLink(link);
             app.setStatus(Status.approved);
             das.save(app);
@@ -86,25 +73,21 @@ public class DoctorController {
     @PostMapping ("/addPrescription")
     public String addPrescription(@ModelAttribute("selected") Appointment a)
     {
-        id=a.getId();
+        pcs = new PrescriptionCreationSystem(ar, pr);
+        pcs.setId(a.getId());
         return "redirect:/prescriptionDetails";
     }
     @PostMapping("/prescriptionDetails")
-    public String PrescriptionDetails(@RequestParam ("medication") String m, @RequestParam ("diagnosis") String d)
+    public String PrescriptionDetails(@RequestParam ("medication") String m, @RequestParam("diagnosis") String d, @RequestParam("password") String pres)
     {
-        if (d.equals("") || m.equals(""))
+        pcs.setPassword(pres);
+        pcs.setMedication(m);
+        pcs.setUsername(Visitor.currentUser);
+        pcs.setDiagnosis(d);
+        if(!pcs.checkAttr())
         {
-            Visitor.errorMessage="Field left empty";
             return "redirect:/problem";
         }
-
-        Optional<Appointment> a=ar.findById(id);
-        Appointment app=a.get();
-        Prescription p= new Prescription(d,m,app);
-        app.setStatus(Status.removed);
-        pr.save(p);
-        app.setPrescription(p);
-        ar.save(app);
         return "redirect:/addPrescription";
     }
 
--- src/main/java/tele/doc/project/systems/others/PrescriptionCreationSystem.java
+++ src/main/java/tele/doc/project/systems/others/PrescriptionCreationSystem.java
@@ -1,5 +1,92 @@
 package tele.doc.project.systems.others;
 
-public class PrescriptionCreationSystem extends DoctorVerification{
+import tele.doc.project.domain.Appointment;
+import tele.doc.project.domain.Prescription;
+import tele.doc.project.domain.Status;
+import tele.doc.project.domain.Visitor;
+import tele.doc.project.repositories.AppointmentRepository;
+import tele.doc.project.repositories.PrescriptionRepository;
 
+import javax.net.ssl.SSLServerSocket;
+import java.util.Optional;
+
+public class PrescriptionCreationSystem extends DoctorVerification{
+    private String username;
+    private String password;
+    private String medication;
+    private Long id;
+    private String diagnosis;
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public void setDiagnosis(String diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+
+    AppointmentRepository ar;
+    PrescriptionRepository pr;
+
+    public PrescriptionCreationSystem(AppointmentRepository ar, PrescriptionRepository pr)
+    {
+        this.ar = ar;
+        this.pr = pr;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMedication() {
+        return medication;
+    }
+
+    public void setMedication(String medication) {
+        this.medication = medication;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean checkAttr()
+    {
+        if (diagnosis.equals("") || medication.equals("") || password.equals(""))
+        {
+            Visitor.errorMessage="Field left empty";
+            return false;
+        }
+
+        if(!verifyUser(username,password))
+        {
+            Visitor.errorMessage="Incorrect Password";
+            return false;
+        }
+
+        Optional<Appointment> a=ar.findById(id);
+        Appointment app=a.get();
+        Prescription p= new Prescription(diagnosis,medication,app);
+        app.setStatus(Status.removed);
+        pr.save(p);
+        app.setPrescription(p);
+        ar.save(app);
+        return true;
+    }
 }
